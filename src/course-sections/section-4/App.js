8438ce43611569cd451e901daa5b08c50ae33f25
Simon Winkler <simon.winkler@viu.ch>
2020-07-24 20:06:44 +0200

    Implement basic navigation mechanism in order to navigate between results from different course sections

---
+++ src/course-sections/section-4/App.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+export default function App() {
+  return (
+    <div>App Section 4</div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import App_course_section_4 from './course-sections/section-4/App';
+import App_course_section_x from './course-sections/section-x/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    	<nav>
+        <ul>
+          <li>
+            <Link to="/section-4">Section 4</Link>
+          </li>
+          <li>
+            <Link to="/section-x">Section X</Link>
+          </li>
+        </ul>
+    	</nav>
+      <Switch>
+        <Route path="/section-4">
+          <App_course_section_4 />
+        </Route>
+        <Route path="/section-x">
+          <App_course_section_x />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
