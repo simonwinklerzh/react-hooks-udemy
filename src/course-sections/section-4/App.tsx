6d63966f3b502346c633b69bb7e6c70eeccab604
Simon Winkler <simon.winkler@viu.ch>
2020-07-24 23:21:00 +0200

    Render some of the data we fetch from the api

--- src/course-sections/section-4/App.tsx
+++ src/course-sections/section-4/App.tsx
@@ -1,20 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { handleErrors } from '../../utilities';
 
+export interface INewsListEntry {
+  url?: string;
+  title: string;
+  author: string;
+}
+
+export const NewsListEntry = ({
+  url,
+  title,
+  author
+}: INewsListEntry) => (
+  <li>
+    { url
+      ? <a href={url}>{title}</a>
+      : <span>{title}</span>
+    }
+    &nbsp;
+    <small>Author: <b>{author}</b></small>
+  </li>
+);
+
 export default function App() {
+  const [results, setResults] = useState([]);
+
   useEffect(() => {
     fetch('http://hn.algolia.com/api/v1/search?query=reacthooks')
       .then(handleErrors)
       .then(response => response.json())
       .then(responseObject => {
-        console.log('ok');
-        console.log(responseObject)
+        setResults(responseObject.hits);
       })
       .catch(console.error);
-
-  });
+  }, []);
 
   return (
-    <div>App Section 4</div>
+    <>
+      <div>App Section 4</div>
+      <ul>
+        {results.map((result: INewsListEntry, index) => (
+          <NewsListEntry key={index}
+            url={result.url}
+            title={result.title}
+            author={result.author} />
+        ))}
+      </ul>
+    </>
   );
 }
