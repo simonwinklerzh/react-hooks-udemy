21d2bd3a4f2fdec89e7fb0760e25787103f25679
Simon Winkler <simon.winkler@viu.ch>
2020-07-26 19:05:28 +0200

    Implement first usage of useReducer

--- src/course-sections/section-5/App.tsx
+++ src/course-sections/section-5/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import {
   Container
 } from 'react-bootstrap';
@@ -7,18 +7,61 @@ interface IUserContext {
   userName: string;
 }
 
+interface IState {
+  count: number;
+}
+
+interface IAction {
+  type: string;
+}
+
 const defaultUserContext: IUserContext = {
   userName: 'Simon'
+}
+
+const initialState: IState = {
+  count: 0
+}
+
+const reducer = (state: IState, action: IAction) => {
+  switch(action.type) {
+    case 'increment':
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case 'decrement':
+      return {
+        ...state,
+        count: Math.max(state.count - 1, 0)
+      };
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
 }
 
 const UserContext = React.createContext<IUserContext>(defaultUserContext);
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const userContext: IUserContext = useContext(UserContext);
 
   return (
     <Container>
       <h1>Hello {userContext.userName}</h1>
+      <p><b>Count:</b> {state.count}</p>
+      <button
+        onClick={(e) => dispatch({ type: 'increment' })}
+        type="button">Increment</button>
+      <button
+        disabled={state.count === 0}
+        onClick={(e) => dispatch({ type: 'decrement' })}
+        type="button">Decrement</button>
+      <button
+        onClick={(e) => dispatch({ type: 'reset' })}
+        type="button">Reset</button>
     </Container>
   );
 }
