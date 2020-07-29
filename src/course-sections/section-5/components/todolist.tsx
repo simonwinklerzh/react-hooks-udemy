2cbc3a0a62a8a4018fddfff1684b5d5ad356c7fc
Simon Winkler <simon.winkler@viu.ch>
2020-07-29 09:45:25 +0200

    Add clear completed button to the UI

--- src/course-sections/section-5/components/todolist.tsx
+++ src/course-sections/section-5/components/todolist.tsx
@@ -227,6 +227,26 @@ const FilterButtons = (
   </div>
 );
 
+const ClearButton = (
+  state: TodoStateType,
+  dispatch: React.Dispatch<TodoActions>
+) => (
+  <div className="mt-3 d-flex align-items-end">
+    <Button
+      onClick={(e: React.MouseEvent) => {
+      }}
+      variant="danger"
+      type="button"
+      className={`d-flex align-items-center`}>
+      <svg style={{ width: '14px', height: '14px', marginRight: '4px' }} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-trash2-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2.037 3.225l1.684 10.104A2 2 0 0 0 5.694 15h4.612a2 2 0 0 0 1.973-1.671l1.684-10.104C13.627 4.224 11.085 5 8 5c-3.086 0-5.627-.776-5.963-1.775z"/>
+        <path fillRule="evenodd" d="M12.9 3c-.18-.14-.497-.307-.974-.466C10.967 2.214 9.58 2 8 2s-2.968.215-3.926.534c-.477.16-.795.327-.975.466.18.14.498.307.975.466C5.032 3.786 6.42 4 8 4s2.967-.215 3.926-.534c.477-.16.795-.327.975-.466zM8 5c3.314 0 6-.895 6-2s-2.686-2-6-2-6 .895-6 2 2.686 2 6 2z"/>
+      </svg>
+      Clear completed
+    </Button>
+  </div>
+);
+
 export const TodoList = () => {
   const { state, dispatch } = useContext(AppContext);
   const editTodoInputRef = useRef<HTMLInputElement>(null);
@@ -256,7 +276,10 @@ export const TodoList = () => {
         )
         : null
       }
-      { FilterButtons(state, dispatch) }
+      <div className="d-flex justify-content-between">
+        { FilterButtons(state, dispatch) }
+        { ClearButton(state, dispatch) }
+      </div>
     </Container>
   );
 }
